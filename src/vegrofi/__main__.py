7e013f3168269fa4ed391e4f5ed9624e517d6a4e
Andrey Rybakov <rybakov.ad@icloud.com>
2025-05-27 23:28:12 +0200

    Prepare part of the test logic

---
+++ src/vegrofi/__main__.py
@@ -0,0 +1,194 @@
+# Copyright (c) 2025 Andrey Rybakov
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is furnished
+# to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import os
+import sys
+from argparse import ArgumentParser
+
+from termcolor import colored, cprint
+
+from vegrofi import __release_date__, __version__
+
+
+class FileIsNotValid(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+
+def check_convention(lines, i, error_messages):
+    if i > len(lines) - 5:
+        error_messages.append(
+            f"Expected at least four lines after the line {i+1}, "
+            f"got {len(lines) - i - 1} before the end of file."
+        )
+
+    i += 1
+    line = lines[i].split()
+    if len(line) == 0:
+        error_messages.append(
+            f"Expected at least two words in line {i+1}, got nothing."
+        )
+    elif line[0].lower() != "double-counting":
+        error_messages.append(
+            f'Expected keyword "Double-counting" in line {i+1}, got "{line[0]}".'
+        )
+    elif len(line) == 1:
+        error_messages.append(f"Expected second word in line {i+1}, got nothing.")
+    elif line[1].lower() != "true":
+        error_messages.append(
+            f'Expected keyword value "true" in line {i+1}, got "{line[1]}".'
+        )
+
+    i += 1
+    line = lines[i].split()
+    if len(line) == 0:
+        error_messages.append(
+            f"Expected at least two words in line {i+1}, got nothing."
+        )
+    elif line[0].lower() != "spin-normalized":
+        error_messages.append(
+            f'Expected keyword "Spin-normalized" in line {i+1}, got "{line[0]}".'
+        )
+    elif len(line) == 1:
+        error_messages.append(f"Expected second word in line {i+1}, got nothing.")
+    elif line[1].lower() != "true":
+        error_messages.append(
+            f'Expected keyword value "true" in line {i+1}, got "{line[1]}".'
+        )
+
+    i += 1
+    line = lines[i].split()
+    if len(line) == 0:
+        error_messages.append(
+            f"Expected at least two words in line {i+1}, got nothing."
+        )
+    elif line[0].lower() != "exchange-factor":
+        error_messages.append(
+            f'Expected keyword "Exchange-factor" in line {i+1}, got "{line[0]}".'
+        )
+    elif len(line) == 1:
+        error_messages.append(f"Expected second word in line {i+1}, got nothing.")
+    elif line[1].lower() != "+0.5":
+        error_messages.append(f'Expected value "+0.5" in line {i+1}, got "{line[1]}".')
+
+    i += 1
+    line = lines[i].split()
+    if len(line) == 0:
+        error_messages.append(
+            f"Expected at least two words in line {i+1}, got nothing."
+        )
+    elif line[0].lower() != "on-site-factor":
+        error_messages.append(
+            f'Expected keyword "On-site-factor" in line {i+1}, got "{line[0]}".'
+        )
+    elif len(line) == 1:
+        error_messages.append(f"Expected second word in line {i+1}, got nothing.")
+    elif line[1].lower() != "+1":
+        error_messages.append(f'Expected value "+0.5" in line {i+1}, got "{line[1]}".')
+
+    return i
+
+
+def check_file(filename):
+    error_messages = []
+    found_convention = False
+    found_cell = False
+    found_sites = False
+    found_on_site = False
+    found_exchange = False
+
+    with open(filename, "r") as f:
+        lines = f.readlines()
+
+    i = 0
+    while i < len(lines):
+        if "Hamiltonian convention" in lines[i]:
+            found_convention = True
+            i = check_convention(i=i, lines=lines, error_messages=error_messages)
+
+        if "Cell (Ang)" in lines[i]:
+            found_cell = True
+        if "Magnetic sites" in lines[i]:
+            found_sites = True
+        if "Intra-atomic anisotropy tensor (meV) " in lines[i]:
+            found_on_site = True
+        if "Exchange tensor (meV)" in lines[i]:
+            found_exchange = True
+
+        i += 1
+
+    if found_convention == False:
+        error_messages.append("Section with the convention is not found.")
+    if found_cell == False:
+        error_messages.append("Section with the unit cell is not found.")
+    if found_sites == False:
+        error_messages.append("Section with the magnetic sites is not found.")
+    if found_on_site == False:
+        error_messages.append(
+            "Section with the intra-atomic anisotropy parameters is not found."
+        )
+    if found_exchange == False:
+        error_messages.append("Section with the exchange parameters is not found.")
+
+    return error_messages
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument(
+        "filename", type=str, help="File with spin Hamiltonian produced by GROGU."
+    )
+
+    logo = [
+        "  ██╗   ██╗███████╗ ██████╗ ██████╗  ██████╗ ███████╗██╗",
+        "  ██║   ██║██╔════╝██╔════╝ ██╔══██╗██╔═══██╗██╔════╝██║",
+        "  ██║   ██║█████╗  ██║  ███╗██████╔╝██║   ██║█████╗  ██║",
+        "  ╚██╗ ██╔╝██╔══╝  ██║   ██║██╔══██╗██║   ██║██╔══╝  ██║",
+        "   ╚████╔╝ ███████╗╚██████╔╝██║  ██║╚██████╔╝██║     ██║",
+        "    ╚═══╝  ╚══════╝ ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚═╝",
+        " " * 46 + "▄   ▄     ",
+        f"{f'Version: {__version__}':^46}" + "█▀█▀█     ",
+        f"{f'Relese date: {__release_date__}':^46}" + "█▄█▄█     ",
+        f"{'License: MIT license':^46}" + " ███   ▄▄ ",
+        f"{'Copyright (C): 2025 Andrey Rybakov':^46}" + " ████ █  █",
+        " " * 46 + " ████    █",
+        " " * 46 + " ▀▀▀▀▀▀▀▀ ",
+    ]
+    print("\n".join(logo))
+
+    filename = parser.parse_args().filename
+
+    if not os.path.isfile(filename):
+        cprint(f"File {os.path.abspath(filename)} not found.", color="red")
+        sys.exit(1)
+
+    print(f'Checking a file "{os.path.abspath(filename)}"')
+
+    error_messages = check_file(filename=filename)
+
+    if len(error_messages) == 0:
+        cprint("It is a valid GROGU file.", color="green")
+    else:
+        cprint("\n".join(error_messages), color="red")
+        sys.tracebacklimit = 0
+        raise FileIsNotValid("It is NOT a valid GROGU file.")
+
+
+if __name__ == "__main__":
+    main()
